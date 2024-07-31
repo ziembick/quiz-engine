be1749f70e7cd0d59f6bfe84ed2ded6bc76009e5
Paulo Ziembick <pauloziembick@gmail.com>
2024-07-31 13:21:10 -0300

    score

--- src/app/components/Quiz/index.jsx
+++ src/app/components/Quiz/index.jsx
@@ -1,15 +1,17 @@
-'use client'
+"use client";
 import { useState, useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
 import Question from "../Questions";
-import styles from './quiz.module.css';
+import styles from "./quiz.module.css";
 import Start from "../Start";
+import Score from "../Score";
 
 export default function Quiz() {
   const [questions, setQuestions] = useState([]);
   const [currentQuestionId, setCurrentQuestionId] = useState(1);
   const [progress, setProgress] = useState(0);
   const [showRestart, setShowRestart] = useState(false);
+  const [score, setScore] = useState(0);
 
   useEffect(() => {
     fetch("/questions.json")
@@ -19,6 +21,24 @@ export default function Quiz() {
 
   const handleAnswer = (answer) => {
     const currentQuestion = questions.find((q) => q.id === currentQuestionId);
+    if (currentQuestion) {
+      // Check if the answer is correct
+      const isCorrect = Array.isArray(currentQuestion.answer)
+        ? currentQuestion.answer.includes(answer)
+        : currentQuestion.answer === answer;
+
+      // Update the score
+      if (isCorrect) {
+        setScore((prevScore) => prevScore + 10);
+      } else {
+        setScore((prevScore) => prevScore - 5);
+      }
+
+      // Move to the next question
+      setCurrentQuestionId(currentQuestion.next);
+      setProgress((prev) => prev + 1);
+    }
+
     // Process the answer and update the state as needed
     setCurrentQuestionId(currentQuestion.next);
     setProgress((prev) => prev + 1);
@@ -38,15 +58,15 @@ export default function Quiz() {
     return (
       <div>
         <p>Quiz Completed!</p>
-        <button onClick={handleRestartQuiz}>
-          Restart Quiz
-        </button>
+        <Score score={score} />
+        <button onClick={handleRestartQuiz}>Restart Quiz</button>
       </div>
     );
   }
 
   return (
     <div>
+      <Score score={score} />
       <div className={styles.progress_bar}>
         <div style={{ width: `${(progress / questions.length) * 100}%` }} />
       </div>
@@ -57,9 +77,7 @@ export default function Quiz() {
           onAnswer={handleAnswer}
         />
       </AnimatePresence>
-      <button onClick={handleRestartQuiz}>
-        Restart Quiz
-      </button>
+      <button onClick={handleRestartQuiz}>Restart Quiz</button>
     </div>
   );
 }
---
+++ src/app/components/Score/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Score({ score }) {
+  return (
+    <div>
+      <h1>Score: {score}</h1>
+    </div>
+  );
+}
